ad6a17c80146512a73fa4f53c667e803c3be181d
Joy Reyes <nethsajo98@gmail.com>
2022-01-28 19:33:00 +0800

    add functionalities

--- assets/js/lectures.js
+++ assets/js/lectures.js
@@ -16,16 +16,24 @@ if (navigator.geolocation)
 class App {
   //constructor method is called immediately when a new object is created from this class
   constructor() {
+    this._getPosition();
+
     //The _newWorkout method is basically an event handler function so it's a function that's gonna be called by an event listener
 
     //An event handler function will always have the 'this' keyword of the DOM element onto which it is attached. So in this case, that's gonna be the form element. Inside of the _newWorkout method the 'this' keyword gonna point to form and no longer to the App object
+    form.addEventListener('submit', this._newWorkout);
+
+    //Solution is to bind the this._newWorkout in the current object (App). The 'this' keyword is now points to the current object
     form.addEventListener('submit', this._newWorkout.bind(this));
-    this._getPosition();
+
+    inputType.addEventListener('change', this._toggleElevationField);
   }
 
-  //this._loadMap treated as a regular function call not as a method call. In a regular function call the this keyword is set to undefined
-  //Since this is callback function we are not calling it ourselves. It is the getCurrentPosition function that we'll call 'this' callback function once that it gets the current position of the user
-  //'this' keyword will point to the current object
+  //When the success callback function of getCurrentPosition is only: this._loadMap; the this._loadMap method is actually called by the getCurrentPosition() function not in the current object (App). The this._loadMap treated as a regular function call not as a method call. In a regular function call the this keyword is set to undefined.
+
+  //Since this is callback function we are not calling it ourselves. It is the getCurrentPosition function that we'll call that 'this' callback function once that it gets the current position of the user
+
+  //Solution is to bind the this._loadMap in the current object (App). The 'this' keyword is now points to the current object
   _getPosition() {
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(
@@ -42,7 +50,7 @@ class App {
     //If the callback function is this._showForm will throw an error because it is incorrectly set the 'this' keyword. So the _showForm method is now being used as an event handler function below. So just like in regular JavaScript the 'this' keyword in the _showForm function will then be set to the object onto which the event handler is attached so that's gonna be simply the map itself
     this.#map.on('click', this._showForm);
 
-    //So the solution to that is to bind the 'this' keyword because the 'this' keyword is the App object
+    //So the solution to that is to bind the 'this' keyword, because the 'this' keyword is the App object
     this.#map.on('click', this._showForm.bind(this));
   }
 
@@ -52,5 +60,39 @@ class App {
     inputDistance.focus();
   }
 
-  _newWorkout(e) {}
+  //The _toggleElevationField method actually does not use the 'this' keyword anywhere and it doesn't matter what the 'this' keyword be like so therefore we don't have to bind it manually
+  _toggleElevationField() {
+    inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
+    inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
+  }
+
+  _newWorkout(e) {
+    //Check the inputs. Passing the arguments as rest parameters
+    const validInputs = (...inputs) => {
+      //This will loop over the array and then each of them it will check whether the number is finite or not. The every method will only return true if the value in Number.isFinite(inp) was true for all of them (elements in the array). But if only one of these values in  Number.isFinite(inp) was not finite. If the result was false for one of the elements of the array then every will return false
+      inputs.every(inp => Number.isFinite(inp));
+    };
+
+    // if (
+    //   !Number.isFinite(distance) ||
+    //   !Number.isFinite(duration) ||
+    //   !Number.isFinite(cadence)
+    // )
+    //   return alert('Inputs have to be positive numbers!');
+
+    //OR
+    if (type === 'running') {
+      const cadence = +inputCadence.value;
+      //Check if data is valid
+      if (!validInputs(distance, duration, cadence))
+        //Should happen whenever one of these three is not a number and not when all of them are invalid numbers
+        return alert('Inputs have to be positive numbers!');
+    }
+
+    //Calling this as a method of the 'this' keyword, and besides that we are calling it ourselves not a callback function of any other function in JavaScript. So therefore in this renderWorkoutMarker method will still be point to the current object
+    //Pass the workout object
+    this.renderWorkoutMarker(workout);
+  }
+
+  renderWorkoutMarker(workout) {}
 }
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,63 @@
 'use strict';
 
-// prettier-ignore
-const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+class Workout {
+  date = new Date();
+  id = (Date.now() + '').slice(-10);
+  clicks = 0;
+
+  constructor(coords, distance, duration) {
+    this.coords = coords; //[lat, lng]
+    this.distance = distance; //in km
+    this.duration = duration; //in min
+  }
+
+  _setDescription() {
+    // prettier-ignore
+    const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+    this.description = `${this.type[0].toUpperCase() + this.type.slice(1)} on ${
+      months[this.date.getMonth()]
+    } ${this.date.getDate()}`;
+  }
+
+  click() {
+    this.clicks++;
+  }
+}
+
+class Running extends Workout {
+  type = 'running';
+  constructor(coords, distance, duration, cadence) {
+    super(coords, distance, duration);
+    this.cadence = cadence;
+    this.calculatePace();
+    this._setDescription();
+  }
+
+  calculatePace() {
+    // min/km
+    this.pace = this.duration / this.distance;
+    return this.pace;
+  }
+}
+
+class Cycling extends Workout {
+  type = 'cycling';
+  constructor(coords, distance, duration, elevationGain) {
+    super(coords, distance, duration);
+    this.elevationGain = elevationGain;
+    this.calculateSpeed();
+    this._setDescription();
+  }
+
+  calculateSpeed() {
+    this.speed = this.distance / (this.duration / 60);
+    return this.speed;
+  }
+}
+
+////////////////////////////////////////////
+//Application Architecture
 
 const form = document.querySelector('.form');
 const containerWorkouts = document.querySelector('.workouts');
@@ -14,6 +70,8 @@ const inputElevation = document.querySelector('.form__input--elevation');
 class App {
   #map;
   #mapEvent;
+  #mapZoomLevel = 15;
+  #workouts = [];
 
   constructor() {
     this._getPosition();
@@ -21,6 +79,8 @@ class App {
     form.addEventListener('submit', this._newWorkout.bind(this));
 
     inputType.addEventListener('change', this._toggleElevationField);
+
+    containerWorkouts.addEventListener('click', this._moveToPopup.bind(this));
   }
 
   _getPosition() {
@@ -40,7 +100,7 @@ class App {
     console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
 
     const coords = [latitude, longitude];
-    this.#map = L.map('map').setView(coords, 15);
+    this.#map = L.map('map').setView(coords, this.#mapZoomLevel);
     // console.log(map);
 
     L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
@@ -58,26 +118,88 @@ class App {
     inputDistance.focus();
   }
 
+  _hideForm() {
+    //Empty inputs
+    inputDistance.value =
+      inputDuration.value =
+      inputCadence.value =
+      inputElevation.value =
+        '';
+
+    form.style.display = 'none';
+    form.classList.add('hidden');
+    setTimeout(() => {
+      form.style.display = 'grid';
+    }, 1000);
+  }
+
   _toggleElevationField() {
     inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
     inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
   }
 
   _newWorkout(e) {
+    //Check the inputs. Passing the arguments as rest parameters
+    const validInputs = (...inputs) => {
+      return inputs.every(inp => Number.isFinite(inp));
+    };
+
+    const allPositive = (...inputs) => {
+      return inputs.every(inp => inp > 0);
+    };
+
     e.preventDefault();
-    console.log(this);
 
-    //Clear input fields
-    inputDistance.value =
-      inputDuration.value =
-      inputDuration.value =
-      inputElevation.value =
-        '';
-
-    //Display marker
+    //Get data from form
+    const type = inputType.value;
+    const distance = +inputDistance.value;
+    const duration = +inputDuration.value;
     const { lat, lng } = this.#mapEvent.latlng;
+    let workout;
 
-    L.marker([lat, lng])
+    //If workout running, create running object
+    if (type === 'running') {
+      const cadence = +inputCadence.value;
+      //Check if data is valid
+      if (
+        !validInputs(distance, duration, cadence) ||
+        !allPositive(distance, duration, cadence)
+      )
+        return alert('Inputs have to be positive numbers!');
+
+      workout = new Running([lat, lng], distance, duration, cadence);
+    }
+
+    //If workout cycling, create cycling object
+    if (type === 'cycling') {
+      const elevation = +inputElevation.value;
+
+      if (
+        !validInputs(distance, duration, elevation) ||
+        !allPositive(distance, duration)
+      )
+        return alert('Inputs have to be positive numbers!');
+
+      workout = new Cycling([lat, lng], distance, duration, elevation);
+    }
+
+    //Add new object to workout array
+    this.#workouts.push(workout);
+    console.log(workout);
+
+    //Render workout on map as marker
+    //Pass the workout object
+    this._renderWorkoutMarker(workout);
+
+    //Render workout on list
+    this._renderWorkout(workout);
+
+    //Hide form + clear input fields
+    this._hideForm();
+  }
+
+  _renderWorkoutMarker(workout) {
+    L.marker(workout.coords)
       .addTo(this.#map)
       .bindPopup(
         L.popup({
@@ -85,11 +207,78 @@ class App {
           minWidth: 100,
           autoClose: false,
           closeOnClick: false,
-          className: 'running-popup',
+          className: `${workout.type}-popup`,
         })
       )
-      .setPopupContent('Workout')
+      .setPopupContent(
+        `${workout.type === 'running' ? '🏃‍♂️' : '🚴‍♀️'} ${workout.description}`
+      )
       .openPopup();
+  }
+
+  _renderWorkout(workout) {
+    const html = `
+      <li class="workout workout--${workout.type}" data-id="${workout.id}">
+        <h2 class="workout__title">${workout.description}</h2>
+        <div class="workout__details">
+          <span class="workout__icon">${
+            workout.type === 'running' ? '🏃‍♂️' : '🚴‍♀️'
+          }</span>
+          <span class="workout__value">${workout.distance}</span>
+          <span class="workout__unit">km</span>
+        </div>
+        <div class="workout__details">
+          <span class="workout__icon">⏱</span>
+          <span class="workout__value">${workout.duration}</span>
+          <span class="workout__unit">min</span>
+        </div>
+        <div class="workout__details">
+          <span class="workout__icon">⚡️</span>
+          <span class="workout__value">${
+            workout.type === 'running'
+              ? workout.pace.toFixed(1)
+              : workout.speed.toFixed(1)
+          }</span>
+          <span class="workout__unit">${
+            workout.type === 'running' ? 'min/km' : 'km/h'
+          }</span>
+        </div>
+        <div class="workout__details">
+          <span class="workout__icon">${
+            workout.type === 'running' ? '🦶🏼' : '⛰'
+          }</span>
+          <span class="workout__value">${
+            workout.type === 'running' ? workout.cadence : workout.elevationGain
+          }</span>
+          <span class="workout__unit">${
+            workout.type === 'running' ? 'spm' : 'm'
+          }</span>
+        </div>
+      </li>
+    `;
+
+    form.insertAdjacentHTML('afterend', html);
+  }
+
+  _moveToPopup(e) {
+    //event delegation
+    const workOutElement = e.target.closest('.workout');
+
+    if (!workOutElement) return;
+
+    const workout = this.#workouts.find(
+      work => work.id === workOutElement.dataset.id
+    );
+
+    this.#map.setView(workout.coords, this.#mapZoomLevel, {
+      animate: true,
+      pan: {
+        duration: 1,
+      },
+    });
+
+    //using the public interface
+    workout.click();
   }
 }
 
